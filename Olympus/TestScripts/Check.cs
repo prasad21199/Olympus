2a607f43434ab67fabea06ec51ff395f67338c2f
prasad21199 <prasad21199@gmail.com>
2025-03-15 15:05:33 +0530

    rock

--- Olympus/TestScripts/Check.cs
+++ Olympus/TestScripts/Check.cs
@@ -12,7 +12,7 @@ namespace Olympus.TestScripts
         [Test]
         public void Test()
         {
-            Console.WriteLine("Lets break the product..");
+            Console.WriteLine("If you could smell what the rock is cooking..");
         }
     }
 }
