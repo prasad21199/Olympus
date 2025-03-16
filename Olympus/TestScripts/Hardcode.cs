6c9824fa5aafe915f0845a0af634e64a38b2ee1e
prasad21199 <prasad21199@gmail.com>
2025-03-16 15:43:57 +0530

    new j

--- Olympus/TestScripts/Hardcode.cs
+++ Olympus/TestScripts/Hardcode.cs
@@ -12,27 +12,25 @@ namespace Olympus.TestScripts
     public class Hardcode
     {
 
-        [Test]
         public void CheckProfile()
         {
-            //IWebDriver driver = new ChromeDriver();
-            //WebDriverUtils web = new WebDriverUtils();
-            //web.MaximizeWindow(driver);
-            //web.ImplicitWait(driver , IPathConstant.IMPLICIT_WAIT);
-            //driver.Url = "https://icehrmpro.gamonoid.com/login.php?logout=1";
-            //driver.FindElement(By.XPath("//input[@id='username']")).SendKeys("admin");
-            //driver.FindElement(By.XPath("//input[@id='password']")).SendKeys("admin"+Keys.Enter);
-            //driver.FindElement(By.XPath("//a[@id='employeeLink']")).Click();
-            //driver.FindElement(By.XPath("//span[text()='Add a New Employee']")).Click();
-            //By nation = By.XPath("//div[@class='ant-select-selector']//input[@id='rc_select_3']");
-            //web.SetScrollToElement(driver , driver.FindElement(nation));
+            IWebDriver driver = new ChromeDriver();
+            WebDriverUtils web = new WebDriverUtils();
+            web.MaximizeWindow(driver);
+            web.ImplicitWait(driver, IPathConstant.IMPLICIT_WAIT);
+            driver.Url = "https://icehrmpro.gamonoid.com/login.php?logout=1";
+            driver.FindElement(By.XPath("//input[@id='username']")).SendKeys("admin");
+            driver.FindElement(By.XPath("//input[@id='password']")).SendKeys("admin" + Keys.Enter);
+            driver.FindElement(By.XPath("//a[@id='employeeLink']")).Click();
+            driver.FindElement(By.XPath("//span[text()='Add a New Employee']")).Click();
+            By nation = By.XPath("//div[@class='ant-select-selector']//input[@id='rc_select_3']");
+            web.SetScrollToElement(driver, driver.FindElement(nation));
 
-            //Thread.Sleep(1000);
-            //driver.FindElement(nation).Click();
-            //Thread.Sleep(1000);
-            //driver.FindElement(nation).SendKeys("Other" + Keys.Enter);
-            //Thread.Sleep(1000);
-            Console.WriteLine("Bharath");
+            Thread.Sleep(1000);
+            driver.FindElement(nation).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(nation).SendKeys("Other" + Keys.Enter);
+            Thread.Sleep(1000);
         }
     }
 }
