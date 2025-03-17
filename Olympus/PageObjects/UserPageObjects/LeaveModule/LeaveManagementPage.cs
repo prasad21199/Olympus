9edde0f39be11a17f4cbc7807b081a06f79c6b63
prasad21199 <prasad21199@gmail.com>
2025-03-17 11:35:55 +0530

    ss

--- Olympus/PageObjects/UserPageObjects/LeaveModule/LeaveManagementPage.cs
+++ Olympus/PageObjects/UserPageObjects/LeaveModule/LeaveManagementPage.cs
@@ -19,9 +19,53 @@ namespace Olympus.PageObjects.UserPageObjects.LeaveModule
         private IWebElement applyLeaveBtn;
         [FindsBy(How = How.XPath, Using = "//select[@id='leave_type']")]
         private IWebElement leaveTypeDrpDwn;
-        [FindsBy(How = How.XPath, Using = "//input[@id='date_start']")]
+        [FindsBy(How = How.XPath, Using = "//label[text()='Leave Start Date']/..//i")]
         private IWebElement leaveStratDate;
+        [FindsBy(How = How.XPath, Using = "//label[text()='Leave End Date']/..//i")]
+        private IWebElement leaveEndDate;
+        [FindsBy(How = How.XPath, Using = "//div[@class='datepicker-years']//span")]
+        private IReadOnlyCollection<IWebElement> yearsPick;
+        [FindsBy(How = How.XPath, Using = "//div[@class='datepicker-months']//span")]
+        private IReadOnlyCollection<IWebElement> monthPick;
+        [FindsBy(How = How.XPath, Using = "//div[@class='datepicker-days']//td")]
+        private IReadOnlyCollection<IWebElement> dayPick;
 
         public IWebElement LeaveStratDate { get => leaveStratDate; set => leaveStratDate = value; }
+        public IWebElement ApplyLeaveBtn { get => applyLeaveBtn; set => applyLeaveBtn = value; }
+        public IWebElement LeaveTypeDrpDwn { get => leaveTypeDrpDwn; set => leaveTypeDrpDwn = value; }
+        public IWebElement LeaveEndDate { get => leaveEndDate; set => leaveEndDate = value; }
+        public IReadOnlyCollection<IWebElement> YearsPick { get => yearsPick; set => yearsPick = value; }
+        public IReadOnlyCollection<IWebElement> MonthPick { get => monthPick; set => monthPick = value; }
+        public IReadOnlyCollection<IWebElement> DayPick { get => dayPick; set => dayPick = value; }
+
+        public void StartDate(string year , string month , string day)
+        {
+            leaveStratDate.Click();
+            foreach(WebElement year_ in yearsPick)
+            {
+                if(year_.Equals(year))
+                {
+                    year_.Click();
+                    break;
+                }
+            }
+            foreach(WebElement month_ in monthPick)
+            {
+                if(month_.Equals(month))
+                {
+                    month_.Click();
+                    break;
+                }
+            }
+            foreach(WebElement day_ in dayPick)
+            {
+                if(day_.Equals(month))
+                {
+                    day_.Click();
+                    break;
+                }
+            }
+
+        }
     }
 }
